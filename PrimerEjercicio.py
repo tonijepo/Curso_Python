052075222795ab24c37b2d1e5ca0db385a00c9b1
tonijepo <tonijepo@gmail.com>
2024-04-28 23:21:39 +0200

    Primera Version

---
+++ PrimerEjercicio.py
@@ -0,0 +1,5 @@
+# .git init -> .git add -> .git commit
+# .git push -> Nube
+# .git pull <- Nube
+# Primer commit: git config --global user.email "tonijepo@gmail.com"
+# Primer commit: git config --global user.name "tonijepo"
